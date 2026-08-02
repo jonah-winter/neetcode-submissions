5c879dabbb9a8e14cc8418723f17bf079f3f6e14
Joseph Winter <josephphilipwinter@gmail.com>
2026-08-02 18:55:32 -0400

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if not digits:
+            return [1]
+        cnt: int = 0
+        copy: list = digits
+        fin: list = []
+        copy[-1] += 1
+        for i in copy[::-1]:
+            j = i
+            if cnt > 0:
+                j += 1
+                cnt -= 1
+            if j >= 10:
+                j -= 10
+                cnt += 1
+            fin.append(j)
+        if cnt > 0:
+            fin.append(1)
+        realfin: list = []
+        for i in fin[::-1]:
+            realfin.append(i)
+        return realfin
